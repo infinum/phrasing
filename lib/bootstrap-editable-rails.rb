0dad547617bb2f1d3fa11e20acb95734650dce3d
Damir Svrtan <damir.svrtan@infinum.hr>
2013-09-04 17:18:35 +0200

    Extracted view_helper

--- lib/bootstrap-editable-rails.rb
+++ lib/bootstrap-editable-rails.rb
@@ -1,8 +1,12 @@
-require 'bootstrap-editable-rails/view_helper'
-
 module Bootstrap
   module Editable
     module Rails
+      module ViewHelper
+        # Returns +text+ transformed into HTML suitable for textarea input.
+        def textarea_format(text)
+          html_escape(text).gsub(/\r\n|\r|\n/, '<br>').html_safe
+        end
+      end
       class Engine < ::Rails::Engine
         initializer 'bootstrap-editable-rails' do
           ::ActiveSupport.on_load(:action_view) do
