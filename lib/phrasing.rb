b1e177d1adc04d2af51edebb491733bd06140e24
Damir Svrtan <damir.svrtan@infinum.hr>
2013-09-05 16:41:58 +0200

    Phrasable error handler module added for rendering specific layouts

--- lib/phrasing.rb
+++ lib/phrasing.rb
@@ -3,11 +3,17 @@ require "copycat/implementation"
 require "copycat/routes"
 require "copycat/simple"
 require 'copycat'
+require 'phrasing/phrasable_error_handler'
 require 'bootstrap-editable-rails'
 
 module Phrasing
   module Rails
     class Engine < ::Rails::Engine
+      initializer "phrasing" do
+        ActiveSupport.on_load(:action_controller) do
+          # ActionController::Base.send(:include, PhrasableErrorHandler)
+        end
+      end
     end
   end
 end
