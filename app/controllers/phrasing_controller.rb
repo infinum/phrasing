3ff56ded3f56c2ef653ec6155daa9c9ede912bdd
Damir Svrtan <damir.svrtan@infinum.hr>
2013-09-16 08:42:06 +0200

    Added phrasing controller, gem version 0.9.2

---
+++ app/controllers/phrasing_controller.rb
@@ -0,0 +1,8 @@
+class PhrasingController < ActionController::Base
+
+  def update_phrase
+
+    redirect_to copycat_translations_path
+  end
+
+end
--- lib/copycat/routes.rb
+++ lib/copycat/routes.rb
@@ -1,10 +1,7 @@
 module Copycat
   def self.routes(mapper)
     mapper.instance_eval do
-      resources Copycat.route,
-      :as => 'copycat_translations',
-      :controller => 'copycat_translations',
-      :only => [:index, :edit, :update, :destroy] do
+      resources Copycat.route, :as => 'copycat_translations', :controller => 'copycat_translations', :only => [:index, :edit, :update, :destroy] do
         collection do
           get 'help'
           get 'import_export'
@@ -14,5 +11,14 @@ module Copycat
         end
       end
     end
+
+    mapper.instance_eval do 
+      resources CopyCat.route, as: 'phrasing', controller: 'phrasing_controller' do
+        collection do
+          post 'update_phrase'
+        end
+      end
+    end
+
   end
 end
