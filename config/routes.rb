4a61ab83377fe8189a2fb49cdc38c51b1d9555f9
Damir Svrtan <damir.svrtan@infinum.hr>
2013-09-16 09:56:11 +0200

    Routes fix #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  #Copycat.routes(self)
-  resources :copycat
+  # Copycat.routes(self)
+  resources Copycat.route, :as => 'copycat_translations', :controller => 'copycat_translations', :only => [:index, :edit, :update, :destroy] do
+    collection do
+      get 'help'
+      get 'import_export'
+      get 'sync'
+      get 'download'
+      post 'upload'
+    end
+  end
+  get 'phrasing/update_phrase', to: 'phrasing#update_phrase'
 end
