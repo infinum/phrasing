c667ba2df3da384344dddeef4ba7c421ead6d4a4
Damir Svrtan <damir.svrtan@infinum.hr>
2013-09-18 09:02:46 +0200

    options[:as] ||= 'textarea'

--- app/helpers/inline_helper.rb
+++ app/helpers/inline_helper.rb
@@ -20,7 +20,7 @@ module InlineHelper
       return object.send(field_name).to_s.html_safe
     end
 
-    options[:as] = "textarea" if options[:as].blank?
+    options[:as] ||= "textarea"
       
     options[:url] ||= phrasing_polymorphic_url(object, field_name)
 
