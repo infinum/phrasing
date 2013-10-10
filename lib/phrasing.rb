35a09876d3550c3a57886cbe8f1595a89d4cad70
Damir Svrtan <damir.svrtan@infinum.hr>
2013-10-10 12:56:31 +0200

    AssetsFix#3

--- lib/phrasing.rb
+++ lib/phrasing.rb
@@ -7,8 +7,8 @@ module Phrasing
   module Rails
     class Engine < ::Rails::Engine
       initializer :assets, :group => :all do
-        # ::Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-        ::Rails.application.config.assets.precompile += %w( fonts/*.svg fonts/*.eot fonts/*.woff fonts/*.ttf )
+        ::Rails.application.config.assets.paths << ::Rails.root.join('app', 'assets', 'fonts')
+        ::Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
         ::Rails.application.config.assets.precompile += ['editor.js', 'phrasing_engine.css']
       end
       initializer "phrasing" do
