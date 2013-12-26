bff7af5e77b4480b6a0f9677c4665ec70bcfa30d
damir <damir.svrtan@gmail.com>
2013-12-27 00:07:36 +0100

    Moved supress_log functionality to the phrasing class instead of a seperate config file

--- lib/phrasing.rb
+++ lib/phrasing.rb
@@ -23,10 +23,17 @@ module Phrasing
   mattr_accessor :allow_update_on_all_models_and_attributes
   mattr_accessor :route
   mattr_accessor :staging_server_endpoint
-  mattr_accessor :log
   
   @@route = 'phrasing'
-  @@log = false
+
+  def self.log
+    @@log
+  end
+
+  def self.log=(log_value)
+    @@log = log_value
+    supress_log if log_value == false
+  end
 
   def self.setup
     yield self
@@ -49,5 +56,24 @@ module Phrasing
   def self.is_whitelisted?(klass,attribute)
     allow_update_on_all_models_and_attributes == true or whitelist.include? "#{klass}.#{attribute}"
   end
+
+  private
+
+    def self.supress_log
+      logger_class = defined?(ActiveSupport::Logger) ? ActiveSupport::Logger::SimpleFormatter : Logger::SimpleFormatter
+
+      logger_class.class_eval do
+
+        alias_method :old_call, :call
+
+        def call(severity, timestamp, progname, msg)
+          unless (msg.include? "SELECT" and (msg.include? "phrasing_phrases" or msg.include? "phrasing_phrase_versions"))
+            old_call(severity, timestamp, progname, msg)
+          end
+        end
+
+      end
+    end
+
 
 end
