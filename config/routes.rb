16212b18ed5078a3d3e04caf9c1af94865d1d7dd
Damir Svrtan <damir.svrtan@gmail.com>
2014-07-29 14:22:43 +0200

    Refactor config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  resources Phrasing.route, as: 'phrasing_phrases', controller: 'phrasing_phrases', only: [:index, :edit, :update, :destroy] do
+  resources Phrasing.route, as: :phrasing_phrases,
+                            controller: :phrasing_phrases,
+                            only: [:index, :edit, :update, :destroy] do
     collection do
-      get 'help'
-      get 'import_export'
-      get 'sync'
-      get 'download'
-      post 'upload'
-      put 'remote_update_phrase'
+      get :help
+      get :import_export
+      get :sync
+      get :download
+      post :upload
+      put :remote_update_phrase
     end
   end
-  resources :phrasing_phrase_versions, as: 'phrasing_phrase_versions', controller: 'phrasing_phrase_versions', only: [:destroy]
+
+  resources :phrasing_phrase_versions, only: :destroy
 end
