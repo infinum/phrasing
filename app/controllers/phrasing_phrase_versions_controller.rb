78ceeb1ac41a8494944ce3ba1b33ffc8baee48dd
Damir Svrtan <damir.svrtan@gmail.com>
2014-09-15 13:32:17 +0200

    Don't use instance variables in controllers if not needed.

--- app/controllers/phrasing_phrase_versions_controller.rb
+++ app/controllers/phrasing_phrase_versions_controller.rb
@@ -1,9 +1,9 @@
 class PhrasingPhraseVersionsController < ActionController::Base
 
   def destroy
-    @phrasing_phrase_version = PhrasingPhraseVersion.find(params[:id])
-    @phrasing_phrase_version.destroy
-    redirect_to edit_phrasing_phrase_path(@phrasing_phrase_version.phrasing_phrase.id)
+    phrase_version = PhrasingPhraseVersion.find(params[:id])
+    phrase_version.destroy
+    redirect_to edit_phrasing_phrase_path(phrase_version.phrasing_phrase.id)
   end
 
 end
