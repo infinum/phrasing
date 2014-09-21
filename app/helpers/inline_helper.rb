962a9c716a669bfd7e9d61782216bbffb5ca28ba
Damir Svrtan <damir.svrtan@gmail.com>
2014-09-21 22:34:01 +0200

    Refactored inline helper and PhrasingPhrase methods.

--- app/helpers/inline_helper.rb
+++ app/helpers/inline_helper.rb
@@ -3,45 +3,48 @@ module InlineHelper
 # phrase("headline", url: www.infinum.co/yabadaba, inverse: true, interpolation: {min: 15, max: 20}, scope: "models.errors")
 
 # Data model phrase
-# phrase(@record, :title, inverse: true, class: phrase-record-title)
+# phrase(record, :title, inverse: true, class: phrase-record-title)
 
   def phrase(*args)
     if args[0].class == String or args[0].class == Symbol
       key, options = args[0].to_s, args[1]
       phrasing_phrase(key,options || {})
     else
-      record, field_name, options = args[0], args[1], args[2]
-      inline(record, field_name, options || {})
+      record, attribute, options = args[0], args[1], args[2]
+      inline(record, attribute, options || {})
     end
   end
-
-  def inline(record, field_name, options={})
-    return record.send(field_name).to_s.html_safe unless can_edit_phrases?
-
-    klass = 'phrasable'
-    klass += ' phrasable_on' if edit_mode_on?
-    klass += ' inverse' if options[:inverse]
-    klass += options[:class] if options[:class]
-
-    url = phrasing_polymorphic_url(record, field_name)
-
-    content_tag(:span, { class: klass, contenteditable: edit_mode_on?, spellcheck: false,   "data-url" => url}) do
-      (record.send(field_name) || record.try(:key)).to_s.html_safe
-    end
-  end
-
-  alias_method :model_phrase, :inline
 
   private
 
-    def phrasing_phrase(key, options = {})
-      key = options[:scope] ? "#{options[:scope]}.#{key}" : key.to_s
-      if can_edit_phrases?
-        @record = PhrasingPhrase.where(key: key, locale: I18n.locale.to_s).first || PhrasingPhrase.search_i18n_and_create_phrase(key)
-        inline(@record, :value, options)
-      else
-        options.try(:[], :interpolation) ? t(key, options[:interpolation]).html_safe : t(key).html_safe
+    def inline(record, attribute, options={})
+      return uneditable_phrase(record, attribute, options) unless can_edit_phrases?
+
+      klass  = 'phrasable'
+      klass += ' phrasable_on' if edit_mode_on?
+      klass += ' inverse'      if options[:inverse]
+      klass += options[:class] if options[:class]
+
+      url = phrasing_polymorphic_url(record, attribute)
+
+      content_tag(:span, { class: klass, contenteditable: edit_mode_on?, spellcheck: false, "data-url" => url}) do
+        (record.send(attribute) || record.try(:key)).to_s.html_safe
       end
+    end
+
+    def phrasing_phrase(key, options = {})
+      key = options[:scope] ? "#{options[:scope]}.#{key}" : key
+      record = PhrasingPhrase.find_phrase(key)
+      inline(record, :value, options)
+    end
+
+    def uneditable_phrase(record, attribute, options={})
+      record_value = if options[:interpolation]
+        I18n.interpolate(record.send(attribute), options[:interpolation])
+      else
+        record.send(attribute)
+      end
+      record_value.to_s.html_safe
     end
 
     def edit_mode_on?
--- app/models/phrasing_phrase.rb
+++ app/models/phrasing_phrase.rb
@@ -1,35 +1,40 @@
 class PhrasingPhrase < ActiveRecord::Base
 
   validates_presence_of :key, :locale
-
   validate :uniqueness_of_key_on_locale_scope, on: :create
 
   has_many :versions, dependent: :destroy, class_name: PhrasingPhraseVersion
 
   after_update :version_it
 
-  def self.search_i18n_and_create_phrase key
-    begin
-      value = I18n.t key, raise: true
-      create_phrase(key, value)
-    rescue I18n::MissingTranslationData
-      create_phrase(key)
-    end
+  def self.find_phrase(key)
+    where(key: key, locale: I18n.locale.to_s).first || search_i18n_and_create_phrase(key)
   end
 
   private
 
-    def self.create_phrase key, value = nil
+    def self.search_i18n_and_create_phrase(key)
+      begin
+        value = I18n.t(key, raise: true)
+        create_phrase(key, value)
+      rescue I18n::MissingTranslationData
+        create_phrase(key)
+      end
+    end
+
+    def self.create_phrase(key, value=nil)
       phrasing_phrase = PhrasingPhrase.new
       phrasing_phrase.locale = I18n.locale.to_s
-      phrasing_phrase.key = key.to_s
-      phrasing_phrase.value = value || key.to_s
+      phrasing_phrase.key    = key.to_s
+      phrasing_phrase.value  = value || key.to_s
       phrasing_phrase.save
       phrasing_phrase
     end
 
     def uniqueness_of_key_on_locale_scope
-      errors.add(:key, "Duplicate entry #{key} for locale #{locale}") unless PhrasingPhrase.where(key: key, locale: locale).empty?
+      if PhrasingPhrase.where(key: key, locale: locale).any?
+        errors.add(:key, "Duplicate entry #{key} for locale #{locale}")
+      end
     end
 
     def version_it
